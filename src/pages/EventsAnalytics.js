c0812481223bacac2d31e87546ebbf22772be875
Yash Shah <yashshh123@gmail.com>
2022-04-21 00:10:10 +0530

    Events Tickets fixes and other Modification.

--- src/pages/EventsAnalytics.js
+++ src/pages/EventsAnalytics.js
@@ -10,23 +10,24 @@ import {
 import React from "react";
 import { useTranslation } from "react-i18next";
 import PeopleIcon from '@material-ui/icons/People';
-import getEvents, { getEventsAnalytics } from "../services/events.service";
+import getEvents, { getEventsAnalytics, getEventsPaymentsAnalytics } from "../services/events.service";
 
 export default function EventsAnalytics() {
   const [events, setEvents] = React.useState([]);
   const [analytics, setAnalytics] = React.useState(null);
-  const [selectedEvent, setSelectedEvent] = React.useState(undefined);
+  const [paymentAnalytics, setPaymentAnalytics] = React.useState(null);
+  const [selectedEvent, setSelectedEvent] = React.useState(1);
   const { t } = useTranslation();
   React.useEffect(() => {
     getEvents().then((res) => {
       setEvents(res);
     });
+    getEventsPaymentsAnalytics().then(res => setPaymentAnalytics(res));
     getAnalytics();
   }, []);
 
   const getAnalytics = (eventId) => {
     getEventsAnalytics(eventId).then((res) => {
-      console.log(res)
       setAnalytics(res)
     })
   }
@@ -60,17 +61,17 @@ export default function EventsAnalytics() {
       </Grid>
       {analytics && <Grid container item={12} spacing={6}>
         <Grid item xs={12} md={3}>
-          <Paper elevation={2} style={{ padding: '20px', borderRadius: '5px' }}>
+          <Paper elevation={2} style={{ padding: '20px', borderRadius: '5px', height: '100%' }}>
             <Grid container spacing={6}>
               <Grid item xs={4} style={{ textAlign: 'center', border: '1px solid rgb(102 100 100)', borderRadius: '15px' }}><PeopleIcon style={{ height: '50px', fontSize: '40px', color: 'rgb(102 100 100)' }} /></Grid>
-              <Grid item xs={8} ><Typography variant="h6">Total Participants</Typography>
+              <Grid item xs={8} ><Typography variant="h6">{t('Analytics.TotalParticipant')}</Typography>
                 <Typography variant="h4">{analytics.total_participant}</Typography></Grid>
             </Grid>
           </Paper>
         </Grid>
         {analytics && analytics.part_option_details.map((part) => (
           <Grid item xs={12} md={3}>
-            <Paper elevation={2} style={{ padding: '20px', borderRadius: '5px' }}>
+            <Paper elevation={2} style={{ padding: '20px', borderRadius: '5px', height: '100%' }}>
               <Grid container spacing={6}>
                 <Grid item xs={4} style={{ textAlign: 'center', border: '1px solid rgb(102 100 100)', borderRadius: '15px' }}><PeopleIcon style={{ height: '50px', fontSize: '40px', color: 'rgb(102 100 100)' }} /></Grid>
                 <Grid item xs={8} >
@@ -86,6 +87,21 @@ export default function EventsAnalytics() {
               </Grid>
             </Paper>
           </Grid>
+        ))}
+        {paymentAnalytics && Object.keys(paymentAnalytics).map((key) => (
+          <Grid item xs={12} md={3}>
+           <Paper elevation={2} style={{ padding: '20px', borderRadius: '5px', height: '100%' }}>
+             <Grid container spacing={6}>
+               <Grid item xs={4} style={{ textAlign: 'center', border: '1px solid rgb(102 100 100)', borderRadius: '15px' }}><PeopleIcon style={{ height: '50px', fontSize: '40px', color: 'rgb(102 100 100)' }} /></Grid>
+               <Grid item xs={8} >
+                 {key === 'total_people_paid' && <Typography variant="h6">{t('Analytics.TotalPaid')}</Typography>}
+                 {key === 'total_people_paid_with_cc' && <Typography variant="h6">{t('Analytics.TotalPaidWithCC')}</Typography>}
+                 {key === 'total_ticket_sold' && <Typography variant="h6">{t('Analytics.TotalSold')}</Typography>}
+                 <Typography variant="h4">{paymentAnalytics[key]}</Typography>
+                </Grid>
+             </Grid>
+           </Paper>
+         </Grid>
         ))}
       </Grid>}
     </Grid>
--- src/services/events.service.js
+++ src/services/events.service.js
@@ -6,4 +6,8 @@ const getEvents = () => {
 export const getEventsAnalytics = (eventId) => {
     return axios.get(`${window.APP_CONFIG.VH_API_BASE_URL}/events/v1/analytics/participants${eventId ? `?event_id=${eventId}` : ''}`).then(res => res.data.data);
 }
+
+export const getEventsPaymentsAnalytics = () => {
+    return axios.get(`${window.APP_CONFIG.VH_API_BASE_URL}/pay/orders/count/0522`).then(res => res.data.data);
+}
 export default getEvents;
