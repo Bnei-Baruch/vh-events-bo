8e9092981b17854479b91341bf205e9fddce4d7c
Yash Shah <yashshh123@gmail.com>
2022-07-28 17:41:43 +0530

    Logs and removal of the https

--- public/config/config.js
+++ public/config/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
     BASE_STATIC_PATH: "/static",
-    VH_API_BASE_URL: "https://api.eurokab.info",
+    VH_API_BASE_URL: "http://api.eurokab.info",
     KEYCLOAK_CONFIG: {
         realm: 'master',
         url: 'https://auth.2serv.eu/auth/',
